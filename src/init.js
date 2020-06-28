fbc4ae6932cc2f2ad9af4374f30415b0f4fde406
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2020-06-27 20:22:35 -0400

    later days commit :sweat_smile:

---
+++ src/init.js
@@ -0,0 +1,25 @@
+import game from './gameState'
+
+const TICK_RATE = 3000;
+
+async function init() {
+  console.log('starting game')
+
+  let nextTimeToTick = Date.now()
+
+  function nextAnimationFrame() {
+    const now = Date.now()
+
+    if (nextTimeToTick <= now) {
+      game.tick()
+      nextTimeToTick = now + TICK_RATE
+    }
+
+    requestAnimationFrame(nextAnimationFrame)
+  }
+
+  requestAnimationFrame(nextAnimationFrame)
+  // can also just call it directly to schedule it once - nextAnimationFrame()
+}
+
+init()
